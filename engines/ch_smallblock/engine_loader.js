2ed9edebb21e1c7437a54763ddc0756578e6ac8b
MCPro1239953 <mcpro1239953@gmail.com>
2025-11-04 19:02:22 -0600

    update some stuff

--- engines/ch_smallblock/engine_loader.js
+++ engines/ch_smallblock/engine_loader.js
@@ -1,13 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let data = [
-    ["2bbl","2x4bbl","FI","4bbl"],
-    [[8,4.125,3.75],[8,3.875,3],[8,3.671,3.1],[8,3.5,3.48],[8,3.75,3],[8,4,3]],
-    ["","F6","F4","T4","39","77","E4","32","V1"],
-    [55,110,200,32,20],
-    // data below
-    [0,0,4,0,52,12,57,2]
-  ]; // something to get ./engines.json
-
+function render_table(data) {
   /*
   JSON format:
 
@@ -46,6 +37,8 @@ document.addEventListener("DOMContentLoaded", function() {
   data.shift(); // remove RPOs
   data.shift(); // remove minimums
 
+  let table = document.getElementsByTagName("table")[0];
+
   for (let idx = 0; idx < data.length; idx++) {
     let row = data[idx];
     
@@ -66,7 +59,87 @@ document.addEventListener("DOMContentLoaded", function() {
     power_rpm += minimum_power_rpm;
     power_rpm *= 100;
     torque += minimum_torque;
-    torque_rpm += minimum_power_rpm;
+    torque_rpm += minimum_torque_rpm;
     torque_rpm *= 100;
+
+    if (rpo == "") {
+      rpo = "-";
+    } else {
+      rpo = "L" + rpo;
+    }
+
+    // add info to table
+
+    let html_row = document.createElement("tr");
+
+    let create_cell = (row, info) => {
+      let cell = document.createElement("td");
+      cell.innerHTML = info;
+      row.appendChild(cell);
+    }
+
+    let cyl = dim[0];
+    let bore = dim[1];
+    let stroke = dim[2];
+    let dis = bore * bore * stroke * Math.PI * cyl / 4;
+
+    let formatter = new Intl.NumberFormat("en-US", {style: "decimal", minimumFractionDigits: 0, maximumFractionDigits: 0});
+
+    create_cell(html_row, year);
+    create_cell(html_row, rpo);
+
+    // BEGIN displacement
+    let cc = formatter.format(dis * 16.387064);
+    let liters = (dis * 0.016387064).toFixed(1);
+    let ci = dis.toFixed(1);
+    create_cell(html_row, `${liters} L (${ci} ci, ${cc} cc)`)
+    // END displacement
+
+    // BEGIN bore
+    let bore_in = bore.toFixed(3);
+    let bore_mm = (bore * 25.4).toFixed(1);
+    create_cell(html_row, `${bore_in}" (${bore_mm} mm)`);
+    // END bore
+
+    // BEGIN stroke
+    let stroke_in = stroke.toFixed(3);
+    let stroke_mm = (stroke * 25.4).toFixed(1);
+    create_cell(html_row, `${stroke_in}" (${stroke_mm} mm)`);
+    // END stroke
+    
+    create_cell(html_row, fuel_sys);
+
+    // BEGIN power
+    let power_hp = power;
+    let power_kw = Math.round(power * 0.7457);
+    let power_ps = Math.round(power * 1.014);
+    create_cell(html_row, `${power_hp} hp (${power_kw} kW, ${power_ps} PS)`);
+    // END power
+
+    create_cell(html_row, `${formatter.format(power_rpm)} RPM`);
+
+    // BEGIN torque
+    let torque_lbft = torque;
+    let torque_nm = Math.round(torque * 1.3558);
+    create_cell(html_row, `${torque_lbft} lbf-ft (${torque_nm} Nm)`);
+    // END torque
+    
+    create_cell(html_row, `${formatter.format(torque_rpm)} RPM`);
+
+    table.appendChild(html_row);
   }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  // let data = [
+  //   ["2bbl","2x4bbl","FI","4bbl"],
+  //   [[8,4.125,3.75],[8,3.875,3],[8,3.671,3.1],[8,3.5,3.48],[8,3.75,3],[8,4,3]],
+  //   ["","F6","F4","T4","39","77","E4","32","V1"],
+  //   [55,110,200,32,20],
+  //   // data below
+  //   [0,0,4,0,52,12,57,2]
+  // ]; // something to get ./engines.json
+  fetch("./engines.json")
+    .then(res => res.json())
+    .then(data => render_table(data));
 });
